ca9549b0da9cf55e4417d4e6458b4809f3228faf
Mark Daniel Edillor <edillormark2@gmail.com>
2024-02-08 00:29:53 +0800

    Setup login pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import "./App.css";
+import Login from "./pages/Login/Login";
+import AdminLogin from "./pages/Login/AdminLogin";
+import RegistrationLogin from "./pages/Login/RegistrationLogin";
+import InventoryLogin from "./pages/Login/InventoryLogin";
+import ReportsLogin from "./pages/Login/ReportsLogin";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Login pages */}
+        <Route path="/" element={<Login />} />
+        <Route path="/login/adminlogin" element={<AdminLogin />} />
+        <Route
+          path="/login/registrationlogin"
+          element={<RegistrationLogin />}
+        />
+        <Route path="/login/inventorylogin" element={<InventoryLogin />} />
+        <Route path="/login/reportslogin" element={<ReportsLogin />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
